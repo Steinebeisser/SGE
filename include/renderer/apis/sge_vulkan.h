206ff3f4cbfcf5606fae1b549c9a74b43311b64a
Paul Geisthardt <paul.geisthardt@sinc.de>
2025-04-03 08:15:03 +0200

    add version creating functions vulkan style

--- include/renderer/apis/sge_vulkan.h
+++ include/renderer/apis/sge_vulkan.h
@@ -11,4 +11,11 @@ typedef struct vulkan_app_info {
         uint32_t        application_version;
 } vulkan_app_info;
 
+typedef struct sge_vulkan_render_settings {
+        SGE_BOOL                enable_validation_layers;
+        SGE_BOOL                use_dynamic_rendering;
+        SGE_BOOL                use_sge_allocator;
+        vulkan_app_info         app_info;
+} sge_vulkan_render_settings;
+
 #endif //SGE_VULKAN_H
