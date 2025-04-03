263d22677866abe06a851263dfe997f5285f3715
Paul Geisthardt <paul.geisthardt@sinc.de>
2025-04-03 13:00:40 +0200

    add other apis for future use

---
+++ include/renderer/apis/sge_opengl.h
@@ -0,0 +1,12 @@
+//
+// Created by Geisthardt on 03.04.2025.
+//
+
+#ifndef SGE_OPENGL_H
+#define SGE_OPENGL_H
+
+typedef struct sge_opengl_render_settings {
+
+} sge_opengl_render_settings;
+
+#endif //SGE_OPENGL_H
