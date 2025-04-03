83a1950b13b9492125aa8e9e2ba3a769515f4681
Steinebeisser <paul.geisthardt@gmx.de>
2025-04-03 13:00:40 +0200

    add other apis for future use

---
+++ include/renderer/apis/sge_directx.h
@@ -0,0 +1,12 @@
+//
+// Created by Geisthardt on 03.04.2025.
+//
+
+#ifndef SGE_DIRECTX_H
+#define SGE_DIRECTX_H
+
+typedef struct sge_directx_render_settings {
+
+} sge_directx_render_settings;
+
+#endif //SGE_DIRECTX_H
