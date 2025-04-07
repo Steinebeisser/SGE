11a37e7dbf0329e9d9b623c9e31d64f75b0f05c9
Steinebeisser <paul.geisthardt@gmx.de>
2025-04-07 16:52:26 +0200

    refactor: move crc32 calculation into own file (#26)

---
+++ src/utils/hash/crc32.c
@@ -0,0 +1,19 @@
+//
+// Created by Geisthardt on 07.04.2025.
+//
+
+#include "utils/hash/crc32.h"
+
+#include <utils/sge_math.h>
+
+uint32_t calculate_crc32(void *data, size_t length) {
+        uint32_t crc = 0xFFFFFFFF;
+        uint8_t *bytes = (uint8_t *)data;
+
+        for (size_t i = 0; i < length; i++) {
+                uint8_t byte = bytes[i];
+                crc = (crc >> 8) ^ crc32_table[(crc & 0xFF) ^ byte];
+        }
+
+        return crc ^ 0xFFFFFFFF;
+}
